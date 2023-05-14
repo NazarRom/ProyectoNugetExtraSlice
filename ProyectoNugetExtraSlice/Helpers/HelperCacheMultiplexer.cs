282c1f28c8316c0f1394aaef36b2079869154ad1
NazarRom <nazarrom321@gmail.com>
2023-05-14 17:36:48 +0200

    commit

---
+++ ProyectoNugetExtraSlice/Helpers/HelperCacheMultiplexer.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using StackExchange.Redis;
+
+namespace MVCApiExtraSlice.Helpers
+{
+    public static class HelperCacheMultiplexer
+    {
+        
+        private static Lazy<ConnectionMultiplexer> CreateConnection =
+        new Lazy<ConnectionMultiplexer>(() =>
+        {
+            var client = new SecretClient(new Uri("https://keyvaultextraslice.vault.azure.net/"), new DefaultAzureCredential());
+            KeyVaultSecret secret = client.GetSecret("cacheredis");
+            string cnn = secret.Value;
+            return ConnectionMultiplexer.Connect(cnn);
+        });
+
+        public static ConnectionMultiplexer GetConnection
+        {
+            get{
+                return CreateConnection.Value;
+            }
+        }
+
+    }
+}
---
+++ ProyectoNugetExtraSlice/Models/UsuarioAux.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiProyectoExtraSlice.Models
+{
+    public class UsuarioAux
+    {
+        public string Nombre_cliente { get; set; }
+       
+        public string Direccion { get; set; }
+       
+        public string Telefono { get; set; }
+       
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
