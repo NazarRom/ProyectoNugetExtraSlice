1f6c9a9bedcbeffbea9d7392b400a585a76533e1
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-06-01 10:57:08 +0200

    01/06/2023

---
+++ ProyectoNugetExtraSlice/Models/ModelSecretKeyDatabase.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiProyectoExtraSlice.Models
+{
+    public class ModelSecretKeyDatabase
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Engine { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string DbName { get; set; }
+    }
+}
