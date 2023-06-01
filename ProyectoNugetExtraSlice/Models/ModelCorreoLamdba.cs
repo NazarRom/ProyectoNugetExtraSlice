2a32c674b5bc7ae79398acc3f26d0dfbbc35352d
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-06-01 11:31:18 +0200

    sube esto

---
+++ ProyectoNugetExtraSlice/Models/ModelCorreoLamdba.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoNugetExtraSlice.Models
+{
+    public class ModelCorreoLamdba
+    {
+        public string Asunto { get; set; }
+
+        public string Email { get; set; }
+
+        public string Body { get; set; }
+
+        public List<Attachment>? Attachments { get; set; }
+    }
+}
